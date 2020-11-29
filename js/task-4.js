838dca4b404c5b6510816265335befd57277be7c
Tetiana Proshyna <mollinne2@gmail.com>
2020-11-29 20:30:21 +0200

    fix task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,14 +1,21 @@
 let credits = 23580;
 const pricePerDroid = 3000;
-const droidAmount = prompt('Please enter number of droids');
+let droidAmount = prompt('Please enter number of droids');
 let message;
-const totalPrice = pricePerDroid * droidAmount;
 if (droidAmount === null) {
   message = 'Отменено пользователем!';
-} else if (totalPrice > credits) {
-  message = 'Недостаточно средств на счету!';
 } else {
-  credits -= totalPrice;
-  message = `Вы купили ${droidAmount} дроидов, на счету осталось ${credits} кредитов.`;
+  droidAmount = Number(droidAmount);
+  const totalPrice = pricePerDroid * droidAmount;
+  const notANumber = Number.isNaN(droidAmount);
+
+  if (totalPrice > credits) {
+    message = 'Недостаточно средств на счету!';
+  } else if (notANumber) {
+    alert('Введено не число, попробуйте еще раз');
+  } else {
+    credits -= totalPrice;
+    message = `Вы купили ${droidAmount} дроидов, на счету осталось ${credits} кредитов.`;
+  }
 }
 console.log(message);
