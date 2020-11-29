4ac913adba8cdb49d30985dd5ad1aa3bd584c487
Tetiana Proshyna <mollinne2@gmail.com>
2020-11-29 22:02:16 +0200

    Update task-6

--- js/task-5.js
+++ js/task-5.js
@@ -1,29 +1,35 @@
-const country = prompt('Please enter delivery country');
+let country = prompt('Please enter delivery country');
 let deliveryPrice;
 let message;
-switch (country.toLowerCase()) {
-  case 'китай':
-    deliveryPrice = '100';
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  case 'чили':
-    deliveryPrice = '250';
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  case 'австралия':
-    deliveryPrice = '170';
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  case 'индия':
-    deliveryPrice = '80';
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  case 'ямайка':
-    deliveryPrice = '120';
-    message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
-    break;
-  default:
-    message = 'В вашей стране доставка не доступна';
-    break;
+if (country === null) {
+  message = 'Отменено пользователем!';
+} else {
+  country =
+    country.charAt(0).toLocaleUpperCase() + country.slice(1).toLowerCase();
+  switch (country) {
+    case 'Китай':
+      deliveryPrice = '100';
+      break;
+    case 'Чили':
+      deliveryPrice = '250';
+      break;
+    case 'Австралия':
+      deliveryPrice = '170';
+      break;
+    case 'Индия':
+      deliveryPrice = '80';
+      break;
+    case 'Ямайка':
+      deliveryPrice = '120';
+      break;
+    default:
+      message = 'В вашей стране доставка не доступна';
+      break;
+  }
 }
+
+if (deliveryPrice) {
+  message = `Доставка в ${country} будет стоить ${deliveryPrice} кредитов`;
+}
+
 alert(message);
